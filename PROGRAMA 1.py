8f9172a0d18507d5fd2d5b5c8c1488defbd61661
RodriguezCarrasco <josuegeovanni2@gmail.com>
2026-04-24 18:18:39 -0600

    Delete PROGRAMA 1.py

--- PROGRAMA 1.py
+++
@@ -1,9 +0,0 @@
-#Mi primer programa en python
-#creado por : NOMBREALUMNO
-
-print("Hola Mundo")
-nombre = input ("Como te llamas?")
-edad = input ("Cuantos años tienes?")
-edad = int(edad); #convierte el texto en Numero
-print("buenas tardes ", nombre)
-print("tienes la siguiente edad ", edad)
